Fix error calculation & other improvements

predict.py
@@ -5,6 +5,7 @@ from dateutil.rrule import *
 from functions import *
 import joblib
 from keras.models import load_model
+from keras.losses import mean_squared_error
 import numpy as np
 import os
 import pandas as pd
@@ -20,6 +21,7 @@ predictLength = int(sum(1 for line in open(dataPath)))
 df = pd.read_csv(dataPath)
 data = df["Scaled"]
 predictData = data[predictLength-pastSteps-timeShift:predictLength-timeShift]
+trueData = data[predictLength-timeShift:predictLength+-timeShift+futureSteps]
 predictData = predictData.to_numpy()
 
 # Load model and predict next *futureSteps* values
@@ -29,7 +31,7 @@ except:
     pass
 
 print("Predicting in progress . . .")
-predictedData = predictData[pastSteps-1:pastSteps]
+predictedData = []
 if modelType == 1:
     predictData = np.reshape(predictData, (1, pastSteps, 1))
     predictedData = np.append(predictedData, model.predict(predictData))
@@ -48,7 +50,7 @@ elif modelType == 3:
         predictedData = np.append(predictedData, model.predict(toPredict))
 else:
     for i in range(0, futureSteps):
-        print("\nPredicting " + str(i+1) + ". step")
+        print("\nPredicting " + str(i+1) + ". step . . .")
         model = load_model(os.path.join(folderPath, "models", "model_" + str(i) + ".h5"))
         toPredict = predictData[:pastSteps]
         toPredict = np.reshape(toPredict, (1, pastSteps, 1))
@@ -74,8 +76,7 @@ saveData = pd.concat([timestamp, pd.DataFrame(predictedData)], axis=1, ignore_in
 saveData.columns = ["Date", "Value"]
 saveData.to_csv(os.path.join(folderPath, "predict.csv"), index=False)
 
-# TODO Fix RMSE calculation & add other error calculations
-# Calculate the RMSE
-# rmse = np.sqrt(((predictedData - predictY) ** 2).mean())
-# print("Calculated RMSE:")
-# print(rmse)
+# Calculate the MSE
+MSE = mean_squared_error(trueData, predictedData).numpy()
+MSE = np.average(MSE)
+print("MSE: " + str(MSE))

train.py
@@ -12,12 +12,12 @@ reload_config()
 from config import *
 # Write important variables to config.py:
 # How many steps to the future we are going to predict
-futureSteps = 5
+futureSteps = 20
 # How many values from the past we are going to use for prediction
-pastSteps = futureSteps*5
+pastSteps = futureSteps*2
 # Time shift to the past; if zero, we are really predicting future data,
 # if it equals futureSteps, we are predicting data prior to today.
-timeShift = futureSteps+50
+timeShift = futureSteps+30
 
 if devMode:
     writevar("futureSteps", futureSteps)
@@ -43,10 +43,10 @@ df["Scaled"] = scaledData
 df.to_csv(dataPath, index=False)
 
 # Define the training dataset and check if the predictLength is positive
-predictLength = int(sum(1 for line in open(dataPath)) - pastSteps)
+predictLength = int(sum(1 for line in open(dataPath)) - 1)
 if predictLength < 0:
     print("Variable *predictLength* isn't positive. Please choose shorter interval.")
-trainData = scaledData[:predictLength - pastSteps]
+trainData = scaledData[predictLength - timeShift - pastSteps*2:predictLength - timeShift]
 trainData = np.reshape(trainData, (len(trainData), 1))
 
 # In trainX will be *pastSteps* values, and in trainY will be wanted value.
@@ -78,19 +78,15 @@ if modelType == 4:
 
 # Build and compile the model, then save it to TICKER dir
 model = Sequential()
-model.add(LSTM(futureSteps*2, return_sequences=True, input_shape=(trainX.shape[1], 1)))
-model.add(Dropout(0.05))
-model.add(LSTM(futureSteps * 3, return_sequences=True))
-model.add(Dropout(0.05))
-model.add(LSTM(futureSteps * 2))
-model.add(Dense(trainY.shape[1]))
-model.compile(optimizer="adam", loss="mean_squared_error")
+model.add(LSTM(1, return_sequences=True, input_shape=(trainX.shape[1], 1), activation="sigmoid"))
+model.add(LSTM(trainY.shape[1], activation="sigmoid"))
+model.compile(optimizer="adadelta", loss="mean_squared_error")
 for i in range(0, trainX.shape[2]):
     usedX = np.reshape(trainX[:, :, i], (trainX.shape[0], trainX.shape[1], 1))
     usedY = np.reshape(trainY[:, :, i], (trainY.shape[0], trainY.shape[1]))
     if modelType == 4:
-        print("\nTraining model for " + str(n+1) + ". step")
-    model.fit(usedX, usedY, validation_split=1, epochs=5)
+        print("\nTraining model for " + str(i+1) + ". step")
+    model.fit(usedX, usedY, validation_split=1, epochs=10, batch_size=1)
     if modelType != 4:
         model.save(os.path.join(folderPath, "models", "model.h5"))
     else: