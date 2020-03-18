54d266bb86e78f23d1b031bba5bf1def7e5267a8
Jason505 <mira.jary@gmail.com>
2020-03-18 11:43:15 +0100

    Package installation fix

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import subprocess
 # Also, if the app path does not exist, it is clear that the app is being run for the first time => install packages
 appPath = os.path.join(os.getenv("APPDATA"), "stopr")
 if not os.path.exists(appPath):
-    os.mkdir(appPath)
+    subprocess.run("python -m pip install --upgrade pip")
     subprocess.run("pip install -r packages.txt")
+    os.mkdir(appPath)
 if os.path.exists("config.py"):
     os.remove("config.py")
 
